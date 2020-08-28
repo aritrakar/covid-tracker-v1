44a015fda45b734b98ab8ebff99d071333c49247
Aritra Kar <aritrakar2002@gmail.com>
2020-08-28 21:01:53 +0530

    Updates

---
+++ precache-manifest.72829fd54ee203ef48dcb41202141677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1167317afb564fd0bfb7faa6b3f9b5e",
+    "url": "/covid-tracker-v1/index.html"
+  },
+  {
+    "revision": "ace9fc04ae7da6e52c44",
+    "url": "/covid-tracker-v1/static/css/main.93167b4c.chunk.css"
+  },
+  {
+    "revision": "08f6db526a2955670f37",
+    "url": "/covid-tracker-v1/static/js/2.778beb91.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-tracker-v1/static/js/2.778beb91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ace9fc04ae7da6e52c44",
+    "url": "/covid-tracker-v1/static/js/main.aa256371.chunk.js"
+  },
+  {
+    "revision": "d72074dfcbadba0c5e5f",
+    "url": "/covid-tracker-v1/static/js/runtime-main.dcd0073a.js"
+  }
+]);
